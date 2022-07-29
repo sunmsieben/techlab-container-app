65af84917f940198d8bf00b26c0c3446adb13fa0
Michael Sieben <michael.sieben@sunwebgroup.com>
2022-07-29 12:13:49 +0200

    Add Frontend backend projects

--- techlab-dapr-frontend/Pages/Index.cshtml.cs
+++ techlab-dapr-frontend/Pages/Index.cshtml.cs
@@ -1,28 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Dapr.Client;
+﻿using Dapr.Client;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace techlab_dapr_frontend.Pages
+namespace techlab_dapr_frontend.Pages;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly ILogger<IndexModel> _logger;
+    private readonly DaprClient _daprClient;
+    public IndexModel(ILogger<IndexModel> logger, DaprClient daprClient)
     {
-        private readonly ILogger<IndexModel> _logger;
-        private readonly DaprClient _daprClient;
-        public IndexModel(ILogger<IndexModel> logger, DaprClient daprClient)
-        {
-            _logger = logger;
-            _daprClient = daprClient;
-        }
+        _logger = logger;
+        _daprClient = daprClient;
+    }
 
-        public async Task OnGet()
-        {
-            _logger.LogInformation("Retrieving weatherforcast");
-            var forecasts = await _daprClient.InvokeMethodAsync<IEnumerable<WeatherForecast>>(
-            HttpMethod.Get,
-            "techlab-dapr-backend",
-            "weatherforecast");
+    public async Task OnGet()
+    {
+        _logger.LogInformation("Retrieving weatherforcast");
+        var forecasts = await _daprClient.InvokeMethodAsync<IEnumerable<WeatherForecast>>(
+        HttpMethod.Get,
+        "techlab-dapr-backend",
+        "weatherforecast");
 
-            ViewData["WeatherForecastData"] = forecasts;
-        }
+        ViewData["WeatherForecastData"] = forecasts;
     }
 }
---
+++ techlab-dapr-servicebus/Controllers/PublishController.cs
@@ -0,0 +1,30 @@
+﻿using Dapr.Client;
+using Google.Type;
+using Microsoft.AspNetCore.Mvc;
+using techlab_dapr_servicebus.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace techlab_dapr_servicebus.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublishController : ControllerBase
+    {
+        // GET: api/<PublishController>
+        [HttpGet]
+        public async Task Get()
+        {
+            var cat = new Cat
+            {
+                Name = "Garfield",
+                Color = "Orange"
+            };
+
+            var daprClient = new DaprClientBuilder().Build();
+
+            await daprClient.PublishEventAsync("cat-pub-sub", "cat", cat);
+            
+        }
+    }
+}
